2794855783c3cc4189822a6767aac82d798aa10e
Theju Prasad <tthejuprasad00@gmail.com>
2023-08-31 22:24:00 +0530

    backend

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://realestate-backend-5ayj.onrender.com",
     issuerBaseURL: "https://dev-t64jbdl1402nen4m.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
